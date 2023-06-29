d821f186cc73bb980cd85f0418e46f8e878a93ed
adzrosalsabilla <adzro.rusya03@gmail.com>
2023-06-29 14:21:24 +0700

    Latihan Modul 08

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('employees')->insert([
+            [
+                'firstname' => 'Purnama',
+                'lastname' => 'Anaking',
+                'email'=> 'purnama.anaking@gmail.com',
+                'age' => 20,
+                'position_id' => 1
+            ],
+
+            [
+                'firstname' => 'Adzro',
+                'lastname' => 'Salsabilla',
+                'email'=> 'adzro.rusya03@gmail.com',
+                'age' => 20,
+                'position_id' => 2
+            ],
+
+            [
+                'firstname' => 'Moch.Aditya',
+                'lastname' => 'Pramana',
+                'email'=> 'adit.pram@gmail.com',
+                'age' => 21,
+                'position_id' => 3
+            ],
+        ]);
+    }
+}
+
+?>
